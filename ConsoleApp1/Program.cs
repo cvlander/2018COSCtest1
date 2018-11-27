a423b311370c6aefdb7700212cca803f1c236d13
programmer <programmer@BB21004>
2018-11-27 13:32:28 -0600

    test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            // This is a test.
+            Console.WriteLine("This is a test.");
         }
     }
 }
